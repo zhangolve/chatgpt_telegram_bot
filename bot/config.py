33b37ec9ce0d31e9b1851f0b834722157fdb75ba
zhangolve <zhangolve@users.noreply.github.com>
2023-03-05 05:34:33 +0000

    update config

--- bot/config.py
+++ bot/config.py
@@ -6,4 +6,4 @@ telegram_token = str(os.environ["telegram_token"])
 openai_api_key = str(os.environ["openai_api_key"])
 use_chatgpt_api = bool(os.environ.get("use_chatgpt_api", True))
 allowed_telegram_usernames = eval(os.environ.get("allowed_telegram_usernames")) if os.environ.get("allowed_telegram_usernames") else []
-new_dialog_timeout = os.environ.get("new_dialog_timeout",600)
+new_dialog_timeout = int(os.environ.get("new_dialog_timeout", 600))
